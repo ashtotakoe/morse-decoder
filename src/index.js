6964ac56452df56fc93962cf811b1af63a09362c
Егор Мартынов <egormartynov@MacBook-Air-Egor.local>
2023-02-09 16:39:02 +0300

    do task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,77 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let count = 0
+    let tempArr = []
+    let bigTempArr = []
+    let bigArr = []
+    let enourmousTempArr = []
+    for (let i = 0; i < expr.length; i++){
+        tempArr.push(expr[i])
+        count++
+        if (count === 10){
+            bigArr.push(tempArr)
+            tempArr = []
+            count = 0
+        }
+    }
+    
+    
+    bigArr.forEach(letter => {
+        let temp = 0
+        bigTempArr = []
+       
+        
+        for (let i = 0; i < letter.length; i++){
+            
+            if (letter[i] === '1'){
+                temp++
+                
+                tempArr.push(letter[i])
+                
+
+                if (letter[i+1] === '0'){
+                    tempArr.push(letter[i+1])
+                    temp++
+                }
+
+                if (temp % 2 == 0){
+                    
+                    bigTempArr.push(tempArr)
+                    tempArr = []
+                    
+                    
+                }
+                
+            }
+            console.log(bigTempArr)
+        }
+        console.log(bigTempArr)
+        enourmousTempArr.push(bigTempArr)
+        
+    });
+    console.log(enourmousTempArr.length)
+    let bigStr = ''
+    enourmousTempArr.forEach(elem => {
+        let str =''
+        console.log(elem)
+        for (let unit of elem){
+            unit = unit.join('')
+            if (unit === '11'){
+                str += '-'
+            }
+            else{
+                str += '.'
+            }
+            console.log(MORSE_TABLE[str])
+            
+        }
+        bigStr += MORSE_TABLE[str]
+        
+    })
+    console.log(bigStr);
+    
+    return bigStr
 }
 
 module.exports = {
